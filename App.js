3ec5373a1bbbbc62c143a8624019fc837366462d
Keith Kurak <keith.kurak@gmail.com>
2017-10-27 14:18:51 -0400

    fixed stuff I did wrong

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NativeDemo from 'react-native-ios-lib-demo';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      demoString: 'Waiting...',
+    };
+  }
+
+  componentDidMount = () => {
+    if (NativeDemo) {
+      NativeDemo.helloWorld().then((result) => {
+        this.setState({
+          demoString: result,
+        });
+      });
+    } else {
+      this.setState({
+        demoString: 'Native package is not available. Did you detach and link?',
+      });
+    }
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{this.state.demoString}</Text>
       </View>
     );
   }
